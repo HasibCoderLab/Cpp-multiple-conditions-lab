84b982ba6d0b9d32fe398fd0d0ce34250e044467
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-02 07:58:38 +0600

    solve

---
+++ threeDigitOrNot.cpp
@@ -0,0 +1,22 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic   : [multiple--conditions]
+//               Problem : 👉👉 [1] 
+//               Want    : 
+// ================================================
+#include <iostream>
+using namespace std;
+int main(){
+    int x ;
+    cout<<"Enter Number ";
+    cin>>x;
+    if (x > 99 && x<1000)
+    {
+        cout<<x <<" is three digit number \n ";
+    }else{
+        cout<<x <<" is not three digit number \n ";
+
+    }
+    
+    return 0 ;
+}
